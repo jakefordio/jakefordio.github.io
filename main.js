c192d77b4e4da0970c7543a8967f6a91bba0b61c
jakefordio <jford.tide@gmail.com>
2023-01-18 19:23:19 -0600

    homepage almost done

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-let menuVisible = false;
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function(docEvent) {
     // toggle side menu
     let collapseButton = document.getElementById('sidebar_button');
     let sidebar = document.getElementById('sidebar');
-    collapseButton.addEventListener('click', (event) => {
+    collapseButton.addEventListener('click', (collEvent) => {
         // open/close the menu
         sidebar.classList.toggle('active');
+        collapseButton.classList.toggle('active');
     });
 }); // end doc ready
